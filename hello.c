7e3b6a48d6668f1015aad24c3a8f09568773eeff
BrowarAndrey <andrey.browar@gmail.com>
2017-10-26 10:15:29 +0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
+int calc(int x, int y)
+{
+        return x+y;
+}
 int main()
 {
         printf("Hello World!\n");
         printf("Added new line\n");
+        printf("Calc: 1 + 3 = %d\n", calc(1,3));
         return 0;
 }
