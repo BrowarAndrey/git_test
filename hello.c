356cf08f526eafc2dee0f8bf9e772f85346c7895
BrowarAndrey <andrey.browar@gmail.com>
2017-12-12 09:21:35 +0200

    new feature in calc line

--- hello.c
+++ hello.c
@@ -7,7 +7,7 @@ int main()
 {
         printf("Hello World!\n");
         printf("Added new line\n");
-        printf("Calc: 1 + 3 = %d\n", calc(1,3));
+        printf("Calc: 1 + 3 + 5= %d\n", calc(1,3)+5);
         printf("PASSED\n");
         return 0;
 }
