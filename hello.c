eaf9b19190bb08be9a912a0e7f4b27bf6b8337b8
BrowarAndrey <andrey.browar@gmail.com>
2018-01-23 10:57:21 +0200

    Update hello.c

--- hello.c
+++ hello.c
@@ -8,6 +8,6 @@ int main()
         printf("Hello World!\n");
         printf("Added new line\n");
         printf("Calc: 1 + 3 + 5= %d\n", calc(1,3)+5);
-        printf("--PASSED--\n");
+        printf("--<PASSED>--\n");
         return 0;
 }
